5e9d82339a5d4ccbf9dfedfe8f06ebd69ea1ab83
Svixdream <Svixdream@gmail.com>
2024-02-15 10:59:11 +0300

    feat: operators

---
+++ Practical-tasks/basics_js/introduction_to_operators/js/js.js
@@ -0,0 +1,16 @@
+/* const 
+    nowYear = 2024
+    ageVladimir = nowYear - 2000
+    ageAlexandra = nowYear - 2001
+
+console.log(ageVladimir + ageAlexandra) */
+
+/* console.log(8 ** 3)
+
+console.log("Hello " + "world") */
+
+let a = 10
+++a
+// a += 10
+
+console.log(a)
