28251cb8ee3caecc1442c162efa6cc5c4d5a8e82
Svixdream <Svixdream@gmail.com>
2024-02-25 09:54:40 +0300

    feat: love randomizer

---
+++ Independent-tasks/02-love_generator/start/js.js
@@ -0,0 +1,6 @@
+const firstName = prompt("Напишите первое имя"),
+    secondName = prompt("Напишите второе имя")
+
+const randomNumber = (Math.floor(Math.random() * 100))
+
+alert(`Результат совместимости имён ${firstName} и ${secondName} равен ${randomNumber}`)
