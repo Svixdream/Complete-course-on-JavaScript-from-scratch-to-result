cf0efc02e48214634cedd1d33eba3766e28cff56
Svixdream <Svixdream@gmail.com>
2024-02-26 12:49:01 +0300

    feat: calc

---
+++ Practical-tasks/functions_and_objects/functions/js.js
@@ -0,0 +1,30 @@
+'use strict'
+
+function calc(apple, b, c, d) {
+
+    return apple + b - c
+}
+
+const result = calc(23, 16, 8)
+
+console.log(result)
+
+/* function calc() {
+
+    return 5 + 5
+}
+
+const result = calc()
+
+console.log(result) */
+
+/* function multiConsole() {
+    console.log("Привет мир")
+    console.log("Привет мир")
+    console.log("Привет мир")
+    console.log("Привет мир")
+    console.log("Привет мир")
+    console.log("Привет мир")
+    console.log("Привет мир")
+}
+multiConsole() */
